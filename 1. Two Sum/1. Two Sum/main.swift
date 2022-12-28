a75e0f32513d3cafd695610741f44635678000c2
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-12-28 13:49:12 +0400

    added 1

---
+++ 1. Two Sum/1. Two Sum/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  1. Two Sum
+//
+//  Created by Артем Чаадаев on 28.12.2022.
+//
+
+import Foundation
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var someInts = [Int]()
+    var mappedInts = [Int: Int]()
+    
+    for (index, value) in nums.enumerated() {
+        if mappedInts.keys.contains(target-value) {
+            someInts += [index, mappedInts[target-value]!]
+        }
+        
+        mappedInts[value] = index
+    }
+    
+    return someInts
+}
+
+print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([3, 2, 4], 6))
+print(twoSum([3, 3], 6))
+print(twoSum([-1, -2, -3, -4, -5], -8))
