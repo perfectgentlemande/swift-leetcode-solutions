851e950e6744ce361ab191da9522ee984942a6ba
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-02-01 01:19:08 +0400

    added 589 iterative

--- easy/589. N-ary Tree Preorder Traversal/589. N-ary Tree Preorder Traversal/main.swift
+++ easy/589. N-ary Tree Preorder Traversal/589. N-ary Tree Preorder Traversal/main.swift
@@ -22,6 +22,49 @@ func preorder(_ root: Node?) -> [Int] {
     return res
 }
 
+struct Stack {
+    private var items: [Node] = []
+		
+	mutating func pop() -> Node {
+		return items.removeFirst()
+	}
+		
+	mutating func push(_ item: Node) {
+		items.insert(item, at: 0)
+	}
+		
+	func isEmpty() -> Bool {
+		return items.count == 0
+	}
+}
+
+func preorderIterative(_ root: Node?) -> [Int] {
+	var res = [Int]()
+	var s = Stack()
+
+	if root != nil {
+		s.push(root!)
+	}
+
+	var cur = Node(0)
+
+	while !s.isEmpty() {
+		cur = s.pop()
+		
+		res.append(cur.val)
+		var i = cur.children.count - 1
+		while i >= 0 {
+			if cur.children[i] != nil {
+				s.push(cur.children[i])
+			}
+
+			i -= 1
+		}
+	}
+
+	return res
+}	
+
 var root = Node(1)
 root.children = [Node(3), Node(2), Node(4)]
 root.children[0].children = [Node(5), Node(6)]
