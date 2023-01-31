14519bdc6c410515727976b0bb535650bd4f7d6b
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-02-01 01:23:37 +0400

    added iterative 590

--- easy/590. N-ary Tree Postorder Traversal/590. N-ary Tree Postorder Traversal/main.swift
+++ easy/590. N-ary Tree Postorder Traversal/590. N-ary Tree Postorder Traversal/main.swift
@@ -24,6 +24,46 @@ func postorder(_ root: Node?) -> [Int] {
     return res
 }
 
+struct Stack {
+    private var items: [Node] = []
+		
+	mutating func pop() -> Node {
+		return items.removeFirst()
+	}
+		
+	mutating func push(_ item: Node) {
+		items.insert(item, at: 0)
+	}
+		
+	func isEmpty() -> Bool {
+		return items.count == 0
+	}
+}
+
+
+func postorderIterative(_ root: Node?) -> [Int] {
+    var res = [Int]()
+    var s = Stack()
+
+    if root == nil {
+        return res
+    }
+    s.push(root!)
+
+    while !s.isEmpty() {
+        var node = s.pop()
+        res.append(node.val)
+
+        for child in node.children {
+            if child != nil {
+                s.push(child!)
+            }
+        }
+    }
+
+    return res.reversed()
+}
+
 var root = Node(1)
 root.children = [Node(3), Node(2), Node(4)]
 root.children[0].children = [Node(5), Node(6)]
