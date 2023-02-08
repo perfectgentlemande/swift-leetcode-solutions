2f96637a260249fd1a3447773a626ae7f3d146ce
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-02-09 02:49:53 +0400

    added one more iterative 102

--- medium/102. Binary Tree Level Order Traversal/102. Binary Tree Level Order Traversal/main.swift
+++ medium/102. Binary Tree Level Order Traversal/102. Binary Tree Level Order Traversal/main.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+import Foundation
+
 public class TreeNode {
     public var val: Int
     public var left: TreeNode?
@@ -82,29 +84,85 @@ func levelOrder(_ root: TreeNode?) -> [[Int]] {
 }
 
 func traverse(_ node: TreeNode, _ level: Int, _ res: inout [[Int]]) {
-	if res.count == level {
-		res.append([Int]())
-	}
+    if res.count == level {
+        res.append([Int]())
+    }
     
-	if node.left != nil {
-		traverse(node.left!, level+1, res)
-	}
-	if node.right != nil {
-		traverse(node.right!, level+1, res)
-	}
+    if node.left != nil {
+        traverse(node.left!, level+1, &res)
+    }
+    if node.right != nil {
+        traverse(node.right!, level+1, &res)
+    }
 }
 
-func levelOrder(_ root: TreeNode?) -> [[Int]] {
+func levelOrderRecursive(_ root: TreeNode?) -> [[Int]] {
     if root == nil {
         return []
     }
     
     var res = [[Int]]()
-	traverse(root!, 0, res)
+    traverse(root!, 0, &res)
 
-	return res
+    return res
 }
 
+struct Queue {
+    private var items: [TreeNode] = []
+        
+    mutating func remove() -> TreeNode {
+        return items.removeLast()
+    }
+        
+    mutating func add(_ item: TreeNode) {
+        items.insert(item, at: 0)
+    }
+    
+    func isEmpty() -> Bool {
+        return items.count == 0
+    }
 
-print("Hello, World!")
+    func count() -> Int {
+        return items.count
+    }
+}
 
+func levelOrderIterativeOneMore(_ root: TreeNode?) -> [[Int]] {
+    if root == nil {
+        return []
+    }
+
+    var res = [[Int]]()
+    var level = 0
+    var queue = Queue()
+    queue.add(root!)
+
+    while !queue.isEmpty() {
+        res.append([Int]())
+        let levelLen = queue.count()
+
+        for _ in 0...levelLen - 1 {
+            let node = queue.remove()
+            res[level].append(node.val)
+
+            if node.left != nil {
+                queue.add(node.left!)
+            }
+            if node.right != nil {
+                queue.add(node.right!)
+            }
+        }
+
+        level+=1
+    }
+
+    return res
+}
+
+var root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right!.left = TreeNode(15)
+root.right!.right = TreeNode(7)
+var output = levelOrderIterativeOneMore(root)
+print(output)
