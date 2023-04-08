0ac19bc4fb0490be6cebf546b4859f40178b9d8f
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-04-09 00:10:40 +0400

    added 509 with memoization

--- easy/509. Fibonacci Number/509. Fibonacci Number/main.swift
+++ easy/509. Fibonacci Number/509. Fibonacci Number/main.swift
@@ -10,3 +10,22 @@ func fib(_ n: Int) -> Int {
     
     return fib(n - 1) + fib(n - 2)
 }
+
+var fibResults = [Int: Int]()
+
+func fibWithMemoization(_ n: Int) -> Int {
+    if n == 0 {
+        return 0
+    }
+    if n == 1 {
+        return 1
+    }
+    
+    var res = -1
+	if fibResults[n] == nil {
+		res = fibWithMemoization(n-1) + fibWithMemoization(n-2)
+		fibResults[n] = res
+	}
+
+	return fibResults[n]!
+}
