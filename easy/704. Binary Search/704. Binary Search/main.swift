6d5442642e40f9f73c95c44ca87bd4cda90f0ced
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-04-10 21:20:39 +0400

    added 704

---
+++ easy/704. Binary Search/704. Binary Search/main.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func search(_ nums: [Int], _ target: Int) -> Int {
+    var left = 0
+    var right = nums.count - 1
+
+    while left <= right {
+        let mid = (left + right) / 2
+
+        if nums[mid] == target {
+            return mid
+        }
+        if nums[mid] > target {
+            right = mid - 1
+        }
+        if nums[mid] < target {
+            left = mid + 1
+        }
+    }
+
+    return -1
+}
